8e5349fdf01ec4d6f0286585f510f61ae3754b87
radicalDilettante <wonjun92@gmail.com>
2022-12-15 19:56:56 +1300

    feat: setup again

--- 1.Webgl2Context/main.js
+++
@@ -1,7 +0,0 @@
-const canvas = document.getElementById("canvas");
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
-const gl = canvas.getContext("webgl2");
-
-gl.clearColor(1.0, 0.0, 0.0, 1.0);
-gl.clear(gl.DEPTH_BUFFER_BIT | gl.COLOR_BUFFER_BIT);
