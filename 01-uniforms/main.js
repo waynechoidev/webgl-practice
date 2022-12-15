a226dd5659419d1a30c01e3a61a07554efc3a72a
radicalDilettante <wonjun92@gmail.com>
2022-12-15 22:16:57 +1300

    feat: 01-03

--- 01-uniforms/main.js
+++ 01-uniforms/main.js
@@ -8,19 +8,20 @@ gl.clear(gl.DEPTH_BUFFER_BIT | gl.COLOR_BUFFER_BIT);
 const vertexShader = `#version 300 es
 precision mediump float;
 in vec2 position;
-in vec2 texCoords;
-out vec2 textureCoords;
 void main () {
-    gl_Position = vec4(position.x, position.y * -1.0, 0.0, 1.0);
-    textureCoords = texCoords;
+    gl_Position = vec4(position, 0.0, 1.0);
 }
 `;
 
 const fragmentShader = `#version 300 es
 precision mediump float;
+uniform float time;
+uniform vec2 resolution;
 out vec4 color;
 void main() {
-      color = vec4(1.0,0.0,0.0,1.0);
+      vec2 st = gl_FragCoord.xy / resolution;
+      color = vec4(st,0.0,1.0);
+      //color = vec4(st,abs(sin(time)),1.0);
 }
 `;
 
@@ -53,12 +54,22 @@ gl.bindBuffer(gl.ARRAY_BUFFER, null);
 
 gl.useProgram(program);
 
-const kernelWeight = gl.getUniformLocation(program, "kernelWeight");
-const ker = gl.getUniformLocation(program, "uKernel[0]");
+const timer = new Date().getTime();
 
-const position = gl.getAttribLocation(program, "position");
-gl.enableVertexAttribArray(position);
-gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
-gl.vertexAttribPointer(position, 2, gl.FLOAT, gl.FALSE, 0, 0);
+function render() {
+  const seconds = (new Date().getTime() - timer) / 1000;
+  const resolution = gl.getUniformLocation(program, "resolution");
+  gl.uniform2fv(resolution, new Float32Array([canvas.width, canvas.height]));
+  const time = gl.getUniformLocation(program, "time");
+  gl.uniform1f(time, seconds);
 
-gl.drawArrays(gl.TRIANGLES, 0, vertices.length / 2);
+  const position = gl.getAttribLocation(program, "position");
+  gl.enableVertexAttribArray(position);
+  gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
+  gl.vertexAttribPointer(position, 2, gl.FLOAT, gl.FALSE, 0, 0);
+
+  gl.drawArrays(gl.TRIANGLES, 0, vertices.length / 2);
+  requestAnimationFrame(render);
+}
+
+render();
