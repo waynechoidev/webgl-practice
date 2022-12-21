f5f651031c879dd6a5e32879f6b388a219fabaa9
radicalDilettante <wonjun92@gmail.com>
2022-12-21 21:34:37 +1300

    feat: 5-9

---
+++ 06-circle/main.js
@@ -0,0 +1,86 @@
+const canvas = document.getElementById("canvas");
+canvas.width = 600;
+canvas.height = 600;
+const gl = canvas.getContext("webgl2");
+gl.clearColor(1.0, 1.0, 1.0, 1.0);
+gl.clear(gl.DEPTH_BUFFER_BIT | gl.COLOR_BUFFER_BIT);
+
+const vertexShader = `#version 300 es
+precision mediump float;
+in vec2 position;
+void main () {
+    gl_Position = vec4(position, 0.0, 1.0);
+}
+`;
+
+const fragmentShader = `#version 300 es
+precision mediump float;
+uniform float time;
+uniform vec2 resolution;
+
+vec3 circle(vec2 coord, vec2 loc, float r){
+  float d = distance(coord, loc);
+
+  // d = step(r,d);
+  d = smoothstep(r, r+0.02, d);
+  return vec3(d);
+}
+
+out vec4 color;
+void main() {
+  vec2 coord = gl_FragCoord.xy / resolution;
+
+  vec3 colorData = circle(coord, vec2(0.5), 0.3);
+  
+  color = vec4(colorData, 1.0);
+}
+`;
+
+const vs = gl.createShader(gl.VERTEX_SHADER);
+gl.shaderSource(vs, vertexShader);
+gl.compileShader(vs);
+if (!gl.getShaderParameter(vs, gl.COMPILE_STATUS)) {
+  console.error(gl.getShaderInfoLog(vs));
+}
+
+const fs = gl.createShader(gl.FRAGMENT_SHADER);
+gl.shaderSource(fs, fragmentShader);
+gl.compileShader(fs);
+if (!gl.getShaderParameter(fs, gl.COMPILE_STATUS)) {
+  console.error(gl.getShaderInfoLog(fs));
+}
+const program = gl.createProgram();
+gl.attachShader(program, vs);
+gl.attachShader(program, fs);
+gl.linkProgram(program);
+if (!gl.getProgramParameter(program, gl.LINK_STATUS)) {
+  console.error(gl.getProgramInfoLog(program));
+}
+const vertices = [-1, -1, 1, -1, -1, 1, -1, 1, 1, 1, 1, -1];
+
+const buffer = gl.createBuffer();
+gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
+gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
+gl.bindBuffer(gl.ARRAY_BUFFER, null);
+
+gl.useProgram(program);
+
+const timer = new Date().getTime();
+
+function render() {
+  const seconds = (new Date().getTime() - timer) / 1000;
+  const resolution = gl.getUniformLocation(program, "resolution");
+  gl.uniform2fv(resolution, new Float32Array([canvas.width, canvas.height]));
+  const time = gl.getUniformLocation(program, "time");
+  gl.uniform1f(time, seconds);
+
+  const position = gl.getAttribLocation(program, "position");
+  gl.enableVertexAttribArray(position);
+  gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
+  gl.vertexAttribPointer(position, 2, gl.FLOAT, gl.FALSE, 0, 0);
+
+  gl.drawArrays(gl.TRIANGLES, 0, vertices.length / 2);
+  requestAnimationFrame(render);
+}
+
+render();
